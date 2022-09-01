413929faadc298eb923806ff38699eb0bf35de5d
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-09-01 12:35:46 +0300

    renamed model.c to main.c, upt to Mafile and create Makefile in slicing_layer folder

--- inference_gvsoc/slicing.h
+++ inference_gvsoc/slicing.h
@@ -1,4 +1,3 @@
 
-// void slicing(unsigned char * array, unsigned char * tmp, int h, int w);
 void slicing_chw_channel(signed char * Input, signed char * Output, int h, int w, int chnls);
 void slicing_hwc_channel(signed char * Input, signed char * Output, int h, int w, int chnls);
