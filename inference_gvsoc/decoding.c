2f2897eb980eaf9cd8419c36a6dca022aa711a6e
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-09-06 13:58:29 +0300

    decoding layer added

---
+++ inference_gvsoc/decoding.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include "decoding.h"
+#include <stdlib.h>
+
+void decoding(float * Input, tTuple * feature_maps, float * strides, int size){
+
+    int height, width, stride, i = 0;
+    for (int j=0; j < size; j++){
+
+        height = feature_maps[j].h;
+        width = feature_maps[j].w;
+        stride = strides[j];
+
+        for(int h = 0; h < height; h++){
+            for(int w = 0; w < width; w++){
+                
+                Input[i] = (Input[i] + w) * stride;
+                Input[i + 1] = (Input[i + 1] + h) * stride;
+
+                Input[i + 2] = exp(Input[i + 2]) * stride;
+                Input[i + 3] = exp(Input[i + 3]) * stride;
+
+                i += 6;
+            }
+        }
+    }
+}
