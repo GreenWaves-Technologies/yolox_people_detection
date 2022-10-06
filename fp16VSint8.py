2a268d19aae1df29794b30ef6e6c05d26f539e55
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-10-06 18:47:14 +0300

    check dimentions added

--- fp16VSint8.py
+++ fp16VSint8.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle, cv2
 from loguru import logger
 from utils import CustomCOCODaset
-from utils import build_graph, make_parser, onnx_layer_output, clip_stats
+from utils import build_graph, make_parser, onnx_layer_output, clip_stats, check_input_dims
 
 from nntool.utils.stats_funcs import qsnr
 from nntool.graph.types import ConstantInputNode
@@ -38,7 +38,9 @@ if __name__ == "__main__":
     sample = CustomCOCODaset.preproc(sample, input_size=(256, 320))
     sample = CustomCOCODaset.slicing(sample)
 
-    with open(f"./stats_val.pickle", "rb") as f:
+    check_input_dims(graph_fp16, sample)
+
+    with open(f"./data/stats_val.pickle", "rb") as f:
         # stats = pickle.load(f)
         stats = graph_fp16.collect_statistics([sample])
 
@@ -50,7 +52,7 @@ if __name__ == "__main__":
     # quantize graph
     node_options_int8 = {
 
-        'Conv_0': {'scheme' : 'float', 'float_type': 'float16'},
+        # 'Conv_0': {'scheme' : 'float', 'float_type': 'float16'},
         # 'Sigmoid_1': {'scheme' : 'float', 'float_type': 'float16'},
         # 'Mul_2': {'scheme' : 'float', 'float_type': 'float16'},
 
@@ -107,24 +109,24 @@ if __name__ == "__main__":
     outputs_nntool_int8 = graph_int8.execute([sample], quantize=True, dequantize=True)
 
     
-    flaot_names = OrderedDict()
+    float_names = OrderedDict()
     for i, output in enumerate(outputs_nntool_float):
-        flaot_names[graph_fp16[i].name] = output[0]
+        float_names[graph_fp16[i].name] = output[0]
 
     int8_names = OrderedDict() 
     for i, output in enumerate(outputs_nntool_int8):
         int8_names[graph_int8[i].name] = output[0]
 
     # intersection of names in both graphs
-    names = flaot_names.keys() & int8_names.keys()
+    names = float_names.keys() & int8_names.keys()
 
     # calucate qsnr for each layer
-    for name in flaot_names:
+    for name in float_names:
         if name in int8_names :
             if "constant" in name or "weights" in name:
                 continue
-            if flaot_names[name].shape == int8_names[name].shape:
-                qsnr_ = qsnr(flaot_names[name], int8_names[name])
+            if float_names[name].shape == int8_names[name].shape:
+                qsnr_ = qsnr(float_names[name], int8_names[name])
                 logger.info(f"{name}:" + f"\t\t {qsnr_}") 
             else:
                 logger.info(f"{name}: \t\t SHAPE MISMATCH")
