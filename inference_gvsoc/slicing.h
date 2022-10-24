3d6faa98fe20082b4614ee4aa03c56b05685afb5
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-10-24 20:36:24 +0300

    pulled inference_gvsoc from dev_master

---
+++ inference_gvsoc/slicing.h
@@ -0,0 +1,3 @@
+
+void slicing_chw_channel(signed char * Input, signed char * Output, int h, int w, int chnls);
+void slicing_hwc_channel(signed char * Input, signed char * Output, int h, int w, int chnls);
