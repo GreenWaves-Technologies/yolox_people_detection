369b87f4af6168ee319364b6f36ca33db9e0908b
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-11-26 00:43:57 +0300

    working improved slicing layer

--- slicing_layer/slicing.py
+++ slicing_layer/slicing.py
@@ -1,7 +1,4 @@
-from re import S
-from tkinter import E
 import numpy as np
-import os
 from utils import read_bin, save_bin
 
 def slicing_channel_fist(array):
@@ -99,14 +96,82 @@ def slicing_hwc_c_style(array, h, w, channels):
                 else:
                     tmp_array[cur] = array[tmp2]
                     tmp2 += 1 
-                cur += 1
 
+                cur += 1
                 if  cur % comp == 0: 
                     tmp1 += w * channels
                     tmp2 += w * channels
-
+                
     return tmp_array
 
+
+def slicing_hwc_c_style_imp(array, h, w, channels):
+
+    o_h, o_w = h // 2, w // 2
+    o_c = channels * 4
+    output = np.zeros((o_h * o_w * o_c ), dtype=np.uint8)
+
+    for j in range(0, o_h):
+        for i in range(0, o_w):
+            for c in range(0, channels): 
+                output[j * o_w * o_c + i * o_c + c]     = array[(j * 2 * w * channels) + (i * 2 * channels) + c]
+                output[j * o_w * o_c + i * o_c + 3 + c] = array[(j * 2 * w * channels) + (i * 2 * channels) + (channels * w) +  c]
+                output[j * o_w * o_c + i * o_c + 6 + c] = array[(j * 2 * w * channels) + (i * 2 * channels) + channels  + c]
+                output[j * o_w * o_c + i * o_c + 9 + c] = array[(j * 2 * w * channels) + (i * 2 * channels) + (channels * w + channels)  + c]
+    return output 
+
+def check_idex(input_array, input_seq, h, w, channels): 
+
+    o_h, o_w = h // 2, w // 2
+    o_c = channels * 4
+    print("input array shape", input_array.shape)
+    output = np.zeros((o_h, o_w, o_c), dtype=np.uint8)
+    output_seq = np.zeros((input_seq.shape), dtype=np.uint8)
+
+    for j in range(0, o_h):
+        for i in range(0, o_w):
+            for c in range(0, channels): 
+
+                output[j, i, c]     = input_array[j * 2, i * 2, c]
+                output[j, i, 3 + c] = input_array[j * 2 + 1, i * 2, c]
+                output[j, i, 6 + c] = input_array[j * 2, i * 2 + 1, c]
+                output[j, i, 9 + c] = input_array[1 + j * 2, 1 + i * 2, c]
+            
+                output_seq[j * o_w * o_c + i * o_c + c]     = input_seq[(j * 2 * w * channels) + (i * 2 * channels) + c]
+                output_seq[j * o_w * o_c + i * o_c + 3 + c] = input_seq[(j * 2 * w * channels) + (i * 2 * channels) + (channels * w) +  c]
+                output_seq[j * o_w * o_c + i * o_c + 6 + c] = input_seq[(j * 2 * w * channels) + (i * 2 * channels) + channels  + c]
+                output_seq[j * o_w * o_c + i * o_c + 9 + c] = input_seq[(j * 2 * w * channels) + (i * 2 * channels) + (channels * w + channels)  + c]
+
+
+                # output_seq[j * o_w * 12 + i * 12 + c] = input[j * 2 * w * channels + i * 2 * channels + c]
+                # print(f"idxOutImp = {j * o_w * 12 + i * 12 + c:>2}\tidxOutTrue = {j * 2 * w * channels + i * 2 * channels + c:>2}\ti = {i:>2}\tj = {j:>2}")
+                # idxInp = j * o_w * channels + i * channels + c
+                # print(f"idxOutImp = {j * o_w * 12 + i * 12 + c:>2}\tidxOutTrue = {j * 2 * w * channels + i * 2 * channels + c:>2}\ti = {i:>2}\tj = {j:>2}")
+                # print(f"idxOutImp = {j * o_w * 12 + 3  + i * 12 + 3 + c:>2}\tidxOutTrue = {(1 + j * 2 * w * channels) + i * 2 * channels + c:>2}\ti = {i:>2}\tj = {j:>2}")
+                # print(f"idxOutImp = {(j * o_w * 12 + 6)  + (i * 12 + 6) + c:>2}\tidxOutTrue = {(j * 2 * w * channels + 1) + (i * 2 * channels) + c:>2}\ti = {i:>2}\tj = {j:>2}")
+                # print(f"idxOutImp = {(j * o_w * 12 + 9)  + (i * 12 + 9) + c:>2}\tidxOutTrue = {(1 + j * 2 * w * channels) + (1 + i * 2 * channels) + c:>2}\ti = {i:>2}\tj = {j:>2}")
+
+
+    input_seq = input_seq.reshape((h, w, channels))
+    output_seq = output_seq.reshape((o_h, o_w, o_c))
+
+    cor_output = slicing_channel_last(input_array)
+    print("correct output")
+    # print(input_array)
+    print("my output")
+    print(output)
+    print("=====")
+
+    print("correct output")
+    # print(input_seq)
+    print("my output")
+    print(output_seq)
+    # print("diff sum", np.sum(cor_output - output))
+    # print("diff sum", np.sum(cor_output - output))
+    print("diff")
+    ## flatten and compare
+    # print(np.sum(cor_output.flatten() - output.flatten()))
+    print(np.sum(output - output_seq))
 
 def slicing_hw1_style(array, h, w):
 
@@ -138,9 +203,9 @@ if __name__ == "__main__":
     np.random.seed(1)
 
     ##### main part #####
-    h = 4
-    w = 2
-    c = 3
+    # h = 4
+    # w = 2
+    # c = 3
 
     # array = np.arange(h * w * c).reshape(h, w, c)
     # save_bin(array, f"array_{w}_{h}_{c}.bin")
@@ -155,8 +220,10 @@ if __name__ == "__main__":
 
     # for h in range(2, 400, 2 ):
     #     for w in range(4, 400, 2):
-    h = 2
-    w = 4
+    h = 240  
+    w = 320 
+    # h = 4
+    # w = 4
     c = 3
     array = np.arange(h * w * c, dtype=np.uint64).reshape(h, w, c)
     # array = np.random.randint(low=0, high=255, size=(h * w * c), dtype=np.uint8).reshape(h, w, c )
@@ -180,11 +247,13 @@ if __name__ == "__main__":
     save_bin(array_scliced_python, "array_scliced_python.bin")
     array_sliced_bin = read_bin("array_scliced_python.bin")
 
+
     print(array_sliced_bin)
     print(array_sliced_bin.shape)
 
     # call the fucntion 
-    sliced_c_stype = slicing_hwc_c_style(array_python, h, w, channels=c) 
+    sliced_c_stype = slicing_hwc_c_style_imp(array_python, h, w, channels=c) 
+
     mistach = np.sum(sliced_c_stype - array_sliced_bin)
     print(f"mistach = {mistach} w = {w} h = {h}")
     assert mistach == 0, f"mismatch: {mistach} h={h} w={w} c={c}"
