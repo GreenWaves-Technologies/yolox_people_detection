13aa0b7dd6d65e4b9369e77ee58363ab4e67a046
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-11-26 00:44:29 +0300

    embedded improved new slicing layer

--- inference_gvsoc_240x320_int/slicing.c
+++ inference_gvsoc_240x320_int/slicing.c
@@ -51,7 +51,7 @@ void slicing_chw_channel(char * Input, unsigned char * Output, int h, int w, int
 
 }
 
-void slicing_hwc_channel(char * Input, unsigned char * Output, int h, int w, int channels){
+void slicing_hwc_channel_slow(char * Input, unsigned char * Output, int h, int w, int channels){
 
     unsigned int cur = 0; 
     unsigned int tmp1 = 0;
@@ -89,4 +89,24 @@ void slicing_hwc_channel(char * Input, unsigned char * Output, int h, int w, int
             }
         }
     }
+}
+
+void slicing_hwc_channel(char * Input, unsigned char * Output, int h, int w, int channels){
+
+    unsigned int o_h = h / 2, o_w  =  w / 2; 
+    unsigned int o_c = channels * 4; 
+
+    for(int j = 0; j < o_h; j++){
+        for(int i = 0; i < o_w; i++){
+            for(int c = 0; c < channels; c++){
+                
+                Output[j * o_w * o_c + i * o_c + c]     = Input[(j * 2 * w * channels) + (i * 2 * channels) + c];
+                Output[j * o_w * o_c + i * o_c + 3 + c] = Input[(j * 2 * w * channels) + (i * 2 * channels) + (channels * w) +  c];
+                Output[j * o_w * o_c + i * o_c + 6 + c] = Input[(j * 2 * w * channels) + (i * 2 * channels) + channels  + c];
+                Output[j * o_w * o_c + i * o_c + 9 + c] = Input[(j * 2 * w * channels) + (i * 2 * channels) + (channels * w + channels)  + c];
+
+            }
+        }
+    }
+
 }
