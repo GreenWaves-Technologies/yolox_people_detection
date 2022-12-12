60e1bf2c0dc86e775766f578ab4e9d4b60801a59
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-12-12 10:48:01 +0300

    last changes for creating dumps

--- inference_gvsoc_240x320_int_bayer/val_run.sh
+++ inference_gvsoc_240x320_int_bayer/val_run.sh
@@ -4,11 +4,11 @@ counter=0
 for file in $(ls ./$1)
 do
     echo "./$1/$file" 
-    cp "./$1/$file" ./input_rgb.ppm
+    cp "./$1/$file" ./input.pgm
     make all run platform=gvsoc
 
     mv ./output.bin "./$2/$file.bin"
     counter=$((counter+1))
     echo "$counter : $file FIELS FINISHED"
-    rm ./input_rgb.ppm
+    rm ./input.pgm
 done
