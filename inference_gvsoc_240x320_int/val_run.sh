22605f0a8dbad0ce6a84b49c6c50fecd03fec6e0
Abduragim Shtanchaev <abduragim.shtanchaev@xperience.ai>
2022-12-13 20:47:53 +0300

    MR review fix

--- inference_gvsoc_240x320_int/val_run.sh
+++ inference_gvsoc_240x320_int/val_run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-mkdir -p $2
+DIR_IN=$1
+DIR_OUT=$2
+mkdir -p DIR_IN 
 counter=0
-for file in $(ls ./$1)
+for file in $(ls ./${DIR_IN})
 do
-    echo "./$1/$file" 
-    cp "./$1/$file" ./input_rgb.ppm
+    echo "./${DIR_IN}/$file" 
+    cp "./${DIR_IN}/$file" ./input_rgb.ppm
     make all run platform=gvsoc
 
-    mv ./output.bin "./$2/$file.bin"
+    mv ./output.bin "./${DIR_OUT}/$file.bin"
     counter=$((counter+1))
     echo "$counter : $file FIELS FINISHED"
     rm ./input_rgb.ppm
--- inference_gvsoc_240x320_int_bayer/val_run.sh
+++ inference_gvsoc_240x320_int_bayer/val_run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-mkdir -p $2
+DIR_IN=$1
+DIR_OUT=$2
+mkdir -p DIR_IN 
 counter=0
-for file in $(ls ./$1)
+for file in $(ls ./${DIR_IN})
 do
-    echo "./$1/$file" 
-    cp "./$1/$file" ./input.pgm
+    echo "./${DIR_IN}/$file" 
+    cp "./${DIR_IN}/$file" ./input.pgm
     make all run platform=gvsoc
 
-    mv ./output.bin "./$2/$file.bin"
+    mv ./output.bin "./${DIR_OUT}/$file.bin"
     counter=$((counter+1))
     echo "$counter : $file FIELS FINISHED"
     rm ./input.pgm
