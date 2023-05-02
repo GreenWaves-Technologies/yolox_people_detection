a5d830b9a68776bd664fb54ee4bb5239ff2efcd8
Paul Paillet <paul.paillet@greenwaves-technologies.com>
2023-05-02 11:03:09 +0200

    [FIX] Change pi_task_t (deprecated) w/ pi_evt_t
                                             Changelog: None

--- main_demo.c
+++ main_demo.c
@@ -41,8 +41,8 @@ pi_device_t Ram;
 static struct pi_default_ram_conf ram_conf;
 uint8_t ext_ram_buf;
 
-static pi_task_t ctrl_tasks[2];
-static pi_task_t ram_tasks[2];
+static pi_evt_t ctrl_tasks[2];
+static pi_evt_t ram_tasks[2];
 static int remaining_size;
 static int saved_size = 0;
 static volatile int done = 0;
@@ -72,7 +72,7 @@ static void enqueue_transfer() {
 
     while (remaining_size > 0 && nb_transfers < 2) {
         int iter_size = (remaining_size < ITER_SIZE) ? remaining_size : ITER_SIZE;
-        pi_task_t *task = &ctrl_tasks[current_task];
+        pi_evt_t *task = &ctrl_tasks[current_task];
 
         // Enqueue a transfer. The callback will be called once the transfer is finished
         // so that  a new one is enqueued while another one is already running
@@ -91,7 +91,7 @@ static void handle_transfer_end(void *arg) {
     int current_buff = (int) arg;
 
     enqueue_transfer();
-    pi_task_t *task = &ram_tasks[current_buff];
+    pi_evt_t *task = &ram_tasks[current_buff];
     unsigned char * img = iter_buff[current_buff];
     int rgb_idx=0;
     for (int a = 0; a < 640; a+=2) {
